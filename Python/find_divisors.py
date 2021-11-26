156a08028b6853749918c7c6dfdd795ba15ca2bf
devcrack <jesusaureliohernandez@alumnos.uaslp.edu.mx>
2021-11-25 21:39:08 -0600

    find divisors

---
+++ Python/find_divisors.py
@@ -0,0 +1,33 @@
+"""
+Instructions:
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), 
+from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+"""
+# My solution
+def divisors(n):
+    values = []
+    for i in range (2, n):
+        if n % i == 0:
+            values.append(i)
+    return values if values else f"{n} is prime"
+    return values
+
+if __name__ == "__main__":
+    print(divisors(7))
+
+
+# Test cases
+import codewars_test as test
+from solution import divisors
+
+@test.describe("Fixed Tests")
+def basic_tests():
+    @test.it('Basic Test Cases')
+    def basic_test_cases():
+        test.assert_equals(divisors(15), [3,5])
+        test.assert_equals(divisors(253), [11,23])
+        test.assert_equals(divisors(24), [2,3,4,6,8,12])
+        test.assert_equals(divisors(25), [5])
+        test.assert_equals(divisors(13), "13 is prime")
+        test.assert_equals(divisors(3), "3 is prime")
+        test.assert_equals(divisors(29), "29 is prime")
