11bebab56c432cdc01f0cb809a09767477d47be7
devcrack <jesusaureliohernandez@alumnos.uaslp.edu.mx>
2021-11-27 23:15:29 -0600

    moving zeros

--- Python/moving_zeros_to_end.py
+++ Python/moving_zeros_to_end.py
@@ -1,12 +1,26 @@
-def move_zeros(array):
-    new_array =[]
-    for k in array:    
-        if k == 0:
-            new_array.insert(len(new_array), k)
-        else:
-            new_array.append(k)
-    return new_array
+"""
+Instructions:
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+"""
+
+
+#Clever solution 
+def move_zeros(arr):
+    l = [i for i in arr if isinstance(i, bool) or i!=0]
+    return l+[0]*(len(arr)-len(l))
+
+# My solution 
+def move_zeros(array):    
+    zeros = [k for k in array if not k]
+    array = [k for k in array if k]
+    array += zeros    
+
+    return array
 
 if __name__ == "__main__":
     _array = [1, 2, 0, 1, 0, 1, 0, 3, 0, 1]
     print(move_zeros(_array))
+
+    
